74f377f8638a4c3c586c7b588125fd13cbd68f28
Jafar Sadique <jafar@echologyx.com>
2025-10-28 21:24:10 +0000

    padding removed

--- src/app/admin/problems/new/page.tsx
+++ src/app/admin/problems/new/page.tsx
@@ -129,7 +129,7 @@ export default function NewProblemPage() {
                     highlight={(code) => highlight(code, languages.js, "javascript")}
                     padding={10}
                     style={editorStyles}
-                    className="font-code h-full resize-none text-sm"
+                    className="font-code h-full resize-none text-sm !p-0"
                   />
                 </TabsContent>
                 <TabsContent value="preview" className="mt-2">
