b092b9363d384936f348df25ede740c77ac7faec
Jafar Sadique <jafar@echologyx.com>
2025-10-31 09:35:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5142588860-63faa",
+  "appId": "1:519246521353:web:a0b66e2066bf5be18004d8",
+  "apiKey": "AIzaSyDLMPqq0KdJyAOsk5AZFMuuH5x77H7A1X8",
+  "authDomain": "studio-5142588860-63faa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "519246521353"
+};
